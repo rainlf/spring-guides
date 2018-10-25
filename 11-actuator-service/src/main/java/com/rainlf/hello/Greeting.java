66add9232f017cb3d53265cc93a3c4dfccbf6d6c
Rain <iheyu22@163.com>
2018-10-25 15:02:21 +0800

    Building a RESTful Web Service with Spring Boot Actuator

---
+++ 11-actuator-service/src/main/java/com/rainlf/hello/Greeting.java
@@ -0,0 +1,24 @@
+package com.rainlf.hello;
+
+/**
+ * @author : rain
+ * @date : 2018/10/25 2:36 PM
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
