24ac00ea7d0b8eeb14c50bc51b895f918ffde085
Rain <iheyu22@163.com>
2018-10-26 10:55:33 +0800

    Creating a Batch Service

---
+++ 13-batch-processing/src/main/java/com/rainlf/hello/JobCompletionNotificationListener.java
@@ -0,0 +1,47 @@
+package com.rainlf.hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : rain
+ * @date : 2018/10/26 10:02 AM
+ */
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        log.info("Before job ...");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        log.info("After job ...");
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+
+            jdbcTemplate.query("SELECT first_name, last_name FROM people",
+                    (rs, row) -> new Person(
+                            rs.getString(1),
+                            rs.getString(2))
+            ).forEach(person -> log.info("Found <" + person + "> in the database."));
+
+        }
+    }
+}
---
+++ 13-batch-processing/src/main/java/com/rainlf/hello/Person.java
@@ -0,0 +1,40 @@
+package com.rainlf.hello;
+
+/**
+ * @author : rain
+ * @date : 2018/10/25 4:05 PM
+ */
+public class Person {
+
+    private String lastName;
+    private String firstName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + ", lastName: " + lastName;
+    }
+}
