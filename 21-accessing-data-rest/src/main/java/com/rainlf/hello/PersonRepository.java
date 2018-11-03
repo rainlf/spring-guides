bf6ed5a73f66aabb86985a12bf511a1eb50c1937
Rain <iheyu22@163.com>
2018-11-03 19:13:45 +0800

    Accessing JPA Data with REST

---
+++ 21-accessing-data-rest/src/main/java/com/rainlf/hello/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.rainlf.hello;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * @author : rain
+ * @date : 2018/11/3 7:07 PM
+ */
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+}
