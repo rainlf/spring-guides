9165a835ee221f98e41d4616a0139192f1368909
Rain <iheyu22@163.com>
2018-11-03 19:13:32 +0800

    Enabling Cross Origin Requests for a RESTful Web Service

---
+++ 20-rest-service-cors/src/main/java/com/rainlf/hello/Greeting.java
@@ -0,0 +1,29 @@
+package com.rainlf.hello;
+
+/**
+ * @author : rain
+ * @date : 2018/11/2 11:14 PM
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting() {
+        this.id = -1;
+        this.content = "";
+    }
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ 20-rest-service-cors/src/main/java/com/rainlf/hello/GreetingController.java
@@ -0,0 +1,27 @@
+package com.rainlf.hello;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author : rain
+ * @date : 2018/11/2 11:14 PM
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @CrossOrigin(origins = "http://localhost:9000")
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+        System.out.println("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
