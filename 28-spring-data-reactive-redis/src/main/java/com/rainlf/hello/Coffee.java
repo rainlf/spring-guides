56437e39fc8913511da82e4fc3a1a28488949b39
Rain <iheyu22@163.com>
2018-11-08 13:29:09 +0800

    Accessing Data Reactively with Redis

---
+++ 28-spring-data-reactive-redis/src/main/java/com/rainlf/hello/Coffee.java
@@ -0,0 +1,17 @@
+package com.rainlf.hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : rain
+ * @date : 2018/11/5 5:13 PM
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee {
+    private String id;
+    private String name;
+}
---
+++ 28-spring-data-reactive-redis/src/main/java/com/rainlf/hello/CoffeeController.java
@@ -0,0 +1,25 @@
+package com.rainlf.hello;
+
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author : rain
+ * @date : 2018/11/5 6:27 PM
+ */
+@RestController
+public class CoffeeController {
+    private final ReactiveRedisOperations<String, Coffee> coffeeOps;
+
+    CoffeeController(ReactiveRedisOperations<String, Coffee> coffeeOps) {
+        this.coffeeOps = coffeeOps;
+    }
+
+    @GetMapping("/coffees")
+    public Flux<Coffee> all() {
+        return coffeeOps.keys("*")
+                .flatMap(coffeeOps.opsForValue()::get);
+    }
+}
